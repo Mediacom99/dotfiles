99d7f513d4b97d9e8b74b6d246675292baebc680
Mediacom99 <bertoliedoardo99@gmail.com>
2025-07-11 23:57:43 +0200

    updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,21 +6,43 @@ bind \cg zx-hook
 set -U fish_history_max_size 100000
 set fish_greeting
 
-# Aliases
-alias f="/usr/bin/nvim"
-alias lg="/usr/bin/lazygit"
-alias grep='grep --color=auto'
-alias ls='eza -l --icons --time-style=long-iso --group-directories-first --sort=size'
-alias la='eza -lah --icons --time-style=long-iso --group-directories-first --sort=size'
-alias lt='eza -la --tree -L 2 --icons --time-style=long-iso --group-directories-first --sort=size'
-alias ltn='eza -lah --tree --icons --time-style=long-iso --group-directories-first --sort=size'
-alias update='yay -Syu'
+function f
+    /usr/bin/nvim $argv
+end
 
-# # Export variables
+function lg
+    /usr/bin/lazygit $argv
+end
+
+function grep
+    command grep --color=auto $argv
+end
+
+function ls
+    eza -l --icons --time-style=long-iso --group-directories-first --sort=size $argv
+end
+
+function la
+    eza -lah --icons --time-style=long-iso --group-directories-first --sort=size $argv
+end
+
+function lt
+    eza -la --tree -L 2 --icons --time-style=long-iso --group-directories-first --sort=size $argv
+end
+
+function ltn
+    eza -lah --tree --icons --time-style=long-iso --group-directories-first --sort=size $argv
+end
+
+function update
+    yay -Syu $argv
+end
+
+# Export variables
 set -gx SHELL /usr/bin/fish
 set -gx XDG_CONFIG_HOME /home/mediacom/.config
 set -gx XDG_CACHE_HOME /home/mediacom/.cache
 set -gx EDITOR nvim
 
-# # Append to PATH
+# Append to PATH
 fish_add_path --path /home/mediacom/zig-x86_64-linux-0.14.1 || true
