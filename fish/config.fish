7dc050ea2eb6871621c4c5429fcb7ceab61eb957
Edoardo Bertoli <bertoliedoardo99@gmail.com>
2025-07-11 20:48:41 +0200

    fixed fish config for macos, added fnm (node version manager) setup

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,23 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-bind \cg zx-hook
 set -U fish_history_max_size 100000
 set fish_greeting
+bind \cg zx-hook
 
-# # Append to PATH
+# Append to PATH
 fish_add_path --path /home/mediacom/zig-x86_64-linux-0.14.1 || true
 fish_add_path --path /opt/homebrew/bin || true
+fish_add_path --path /opt/homebrew/sbin || true
+
+fish_add_path /opt/homebrew/opt/postgresql@17/bin
+fish_add_path /opt/homebrew/opt/curl/bin
+fish_add_path /opt/homebrew/opt/binutils/bin
+fish_add_path /opt/homebrew/opt/ccache/libexec
+
+# Bun setup
+set -gx BUN_INSTALL "$HOME/.bun"
+fish_add_path $BUN_INSTALL/bin
 
 # Aliases
 alias f="nvim"
@@ -18,6 +28,7 @@ alias ls='eza -l --icons --time-style=long-iso --group-directories-first --sort=
 alias la='eza -lah --icons --time-style=long-iso --group-directories-first --sort=size'
 alias lt='eza -la --tree -L 2 --icons --time-style=long-iso --group-directories-first --sort=size'
 alias ltn='eza -lah --tree --icons --time-style=long-iso --group-directories-first --sort=size'
+alias brave='/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser'
 
 # Export variables
 set -gx SHELL fish
