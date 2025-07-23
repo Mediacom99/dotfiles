d1d997d59531e054f474957d76d3892383656213
Edoardo Bertoli <bertoliedoardo99@gmail.com>
2025-07-23 09:55:02 +0200

    nvim: add trouble.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -257,7 +257,7 @@ lazy.setup({
                     return 'eslint_d'
                 end
                 vim.env.ESLINT_D_PPID = vim.fn.getpid()
-                vim.env.ESLINT_USE_FLAT_CONFIG = "false" -- Force legacy config by setting false
+                vim.env.ESLINT_USE_FLAT_CONFIG = "true" -- Force legacy config by setting false
                 lint.linters_by_ft = {
                     javascript = { "eslint_d" },
                     typescript = { "eslint_d" },
@@ -308,6 +308,43 @@ lazy.setup({
                     shfmt = {
                         prepend_args = { "-i", "2" },
                     },
+                },
+            },
+        },
+        {
+            "folke/trouble.nvim",
+            opts = {}, -- for default options, refer to the configuration section for custom setup.
+            cmd = "Trouble",
+            keys = {
+                {
+                    "<leader>xx",
+                    "<cmd>Trouble diagnostics toggle<cr>",
+                    desc = "Diagnostics (Trouble)",
+                },
+                {
+                    "<leader>xX",
+                    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                    desc = "Buffer Diagnostics (Trouble)",
+                },
+                {
+                    "<leader>cs",
+                    "<cmd>Trouble symbols toggle focus=false<cr>",
+                    desc = "Symbols (Trouble)",
+                },
+                {
+                    "<leader>cl",
+                    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                    desc = "LSP Definitions / references / ... (Trouble)",
+                },
+                {
+                    "<leader>xL",
+                    "<cmd>Trouble loclist toggle<cr>",
+                    desc = "Location List (Trouble)",
+                },
+                {
+                    "<leader>xQ",
+                    "<cmd>Trouble qflist toggle<cr>",
+                    desc = "Quickfix List (Trouble)",
                 },
             },
         }
