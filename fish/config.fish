7814a44d0101024e46f7be99027bbddb90bf7d9c
Edoardo <edoardo@MacbookPro-Edo.local>
2025-12-10 18:16:01 +0100

    fixed fish config, added ghostty config

--- fish/config.fish
+++ fish/config.fish
@@ -7,19 +7,20 @@ set fish_greeting
 bind \cg zx-hook
 
 # Append to PATH
-fish_add_path --path /home/mediacom/zig-x86_64-linux-0.14.1 || true
+# fish_add_path --path /home/mediacom/zig-x86_64-linux-0.14.1 || true
 fish_add_path --path /opt/homebrew/bin || true
 fish_add_path --path /opt/homebrew/sbin || true
 fish_add_path --path /opt/homebrew/opt/binutils/bin || true
 
-fish_add_path /opt/homebrew/opt/postgresql@17/bin
-fish_add_path /opt/homebrew/opt/curl/bin
-fish_add_path /opt/homebrew/opt/binutils/bin
-fish_add_path /opt/homebrew/opt/ccache/libexec
+# fish_add_path /opt/homebrew/opt/postgresql@17/bin
+# fish_add_path /opt/homebrew/opt/curl/bin
+# fish_add_path /opt/homebrew/opt/binutils/bin
+# fish_add_path /opt/homebrew/opt/ccache/libexec
+fish_add_path /Users/edoardo/.local/bin
 
 # Bun setup
-set -gx BUN_INSTALL "$HOME/.bun"
-fish_add_path $BUN_INSTALL/bin
+# set -gx BUN_INSTALL "$HOME/.bun"
+# fish_add_path $BUN_INSTALL/bin
 
 # Aliases
 alias f="nvim"
@@ -29,11 +30,11 @@ alias ls='eza -l --icons --time-style=long-iso --group-directories-first --sort=
 alias la='eza -lah --icons --time-style=long-iso --group-directories-first --sort=size'
 alias lt='eza -la --tree -L 2 --icons --time-style=long-iso --group-directories-first --sort=size'
 alias ltn='eza -lah --tree --icons --time-style=long-iso --group-directories-first --sort=size'
-alias brave='/Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser'
 
 # Export variables
 set -gx SHELL fish
 set -gx EDITOR nvim
-set -gx LDFLAGS "-L/opt/homebrew/opt/binutils/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/binutils/include"
+set -gx XDG_CONFIG_HOME $HOME/.config
+# set -gx LDFLAGS "-L/opt/homebrew/opt/binutils/lib"
+# set -gx CPPFLAGS "-I/opt/homebrew/opt/binutils/include"
 
